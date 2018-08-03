2eecbe42ffcef4bcdcc045c7ca2abf098adbf15d
msignorini <marco.signorini88@gmail.com>
2018-08-03 18:44:51 +0200

    Commit part 2

---
+++ TrainingDevNet/part_2.py
@@ -0,0 +1,36 @@
+# [] = list
+# () = tuple
+# {} = dictonary
+
+
+# List
+l = [1, "ciao", 7.81]
+print(l)
+l.append("zeta")
+print(l)
+l[1] = 14
+print(l)
+
+# dictonary
+d = {"apples": 5, "pears": 2, "oranges": 9}
+print(d)
+print(d["apples"])
+d["banana"] = 16
+print(d)
+
+
+# loops
+
+for i in range(5):
+    print(i)
+
+for i in l:
+    print(i)
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+# unpacking
+a, b, c = [1, 2, 3]
